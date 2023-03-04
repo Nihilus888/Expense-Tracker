211a217ddbe969d0591006b28b71ca2b0eff57e8
Elliott Tay <austonmartin@gmail.com>
2023-03-05 00:50:56 +0800

    setup commit

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+      <h1>Expense Tracker</h1>
+      <div class="total-expense">$0</div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
